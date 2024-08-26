7dfa5c2d9246a42526ca7aee26941df5374ff792
Juuso <juuso.melentjeff@gmail.com>
2024-08-26 20:35:13 +0300

    feat: add check if someone's birthday and print to console

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from datetime import datetime, date
 from sqlalchemy import create_engine, select, text
+
+today = date.today()
 
 engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/birthdays")
 conn = engine.connect()
 result = conn.execute(text("SELECT * FROM persons;"))
 
+for row in result:
+    (id, first_name, birthday) = row
+
+    if today.month == birthday.month and today.day == birthday.day:
+        print(f"Today is {first_name}'s birthday!")
