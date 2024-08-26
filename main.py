3864e811dddc7ee3c172a9a997ebf6ff1769a203
Juuso <juuso.melentjeff@gmail.com>
2024-08-26 20:58:17 +0300

    feat: use connection object in Python context manager

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from sqlalchemy import create_engine, select, text
 today = date.today()
 
 engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/birthdays")
-conn = engine.connect()
-result = conn.execute(text("SELECT * FROM persons;"))
 
 def is_birthday(birthday):
     return today.month == birthday.month and today.day == birthday.day
 
-for row in result:
-    (id, first_name, birthday) = row
+with engine.connect() as connection:
+    result = connection.execute(text("SELECT * FROM persons;"))
 
-    if is_birthday(birthday):
-        print(f"Today is {first_name}'s birthday!")
+    for row in result:
+        (id, first_name, birthday) = row
+
+        if is_birthday(birthday):
+            print(f"Today is {first_name}'s birthday!")
